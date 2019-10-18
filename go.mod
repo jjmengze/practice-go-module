1812c934db14835a70b1649cadd9cfe2b145d884
MengZn <adnt587@gmail.com>
2019-10-18 10:52:22 +0800

    bump muti langue and go mudule

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/jjmengze/practice-go-module
+module github.com/jjmengze/practice-go-module/v2
 
 go 1.13
--- hi.go
+++ hi.go
@@ -1,8 +1,19 @@
 package hi
 
-import "fmt" 
+import (
+   "errors"
+   "fmt"
+)
 
-func Hi(name string) string {
-   return fmt.Sprintf("Hi, %s", name)
+func Hi(name, lang string) (string, error) {
+   switch lang {
+   case "en":
+      return fmt.Sprintf("Welcome, %s!", name), nil
+   case "tw":
+      return fmt.Sprintf("嗨, %s!", name), nil
+   case "fr":
+      return fmt.Sprintf("Bonjour, %s!", name), nil
+   default:
+      return "", errors.New("unknown language")
+   }
 }
-
